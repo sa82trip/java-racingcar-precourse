1ca0e0f1b09404736d75b773155a9c0ad8ed9efa
Joonsung Gil <sa82trip@gmail.com>
2022-10-09 13:20:08 +0900

    refactor(Identification): Exception message 수정
    Before:
    
    String.format("[ERROR]: %s is not suitable for car name", name));
    
    After:
    
    String.format("%s: %s is not suitable for car name", InfoMessage.ERROR_MESSAGE_BEGIN, name));

--- src/main/java/racingcar/model/Identification.java
+++ src/main/java/racingcar/model/Identification.java
@@ -2,13 +2,15 @@ package racingcar.model;
 
 
 import racingcar.constant.ConstantNumber;
+import racingcar.constant.InfoMessage;
 
 public class Identification {
     private final String name;
 
     public Identification(String name) {
         if (name == null || name.trim().isEmpty() || name.length() > ConstantNumber.MAX_LENGTH_FOR_CAR_NAME) {
-            throw new IllegalArgumentException(String.format("[ERROR]: %s is not suitable for car name", name));
+            throw new IllegalArgumentException(
+                    String.format("%s: %s is not suitable for car name", InfoMessage.ERROR_MESSAGE_BEGIN, name));
         }
         this.name = name;
     }
